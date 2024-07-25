3a29df01f2e4170a41a0a5154c6291ba269c9346
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-26 02:02:34 +0300

    utf validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """ UTF-8 Validation """
+    remaining = 0
+    for num in data:
+        if remaining == 0:
+            if num >> 7 == 0b0:
+                remaining = 0
+            elif num >> 5 == 0b110:
+                remaining = 1
+            elif num >> 4 == 0b1110:
+                remaining = 2
+            elif num >> 3 == 0b11110:
+                remaining = 3
+            else:
+                return False
+        else:
+            if num >> 6 == 0b10:
+                remaining -= 1
+            else:
+                return False
+
+    return remaining == 0
