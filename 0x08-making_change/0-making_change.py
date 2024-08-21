f5e536a63de26390e819fb20afb307cfe5de386b
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-08-22 00:45:31 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,31 +8,39 @@ def makeChange(coins: list, total: int) -> int:
     meet a given amount total"""
     coins.sort()
     counter = {key: 0 for key in coins}
-    current = 0
+    current_sum = 0
     remain = 0
     if total <= 0:
         return 0
+
     for i in range(len(coins) - 1, -1, -1):
         if coins[i] > total:
             continue
 
-        elif coins[i] < total:
-            current += coins[i]
+        if coins[i] == total:
+            current_sum += coins[i]
             counter[coins[i]] += 1
-            remain = total - current
-            while ((remain - coins[i]) > 0):
-                current += coins[i]
+            return sum([count for count in counter.values()])
+
+        elif coins[i] < total and (current_sum + coins[i]) < total:
+            current_sum += coins[i]
+            counter[coins[i]] += 1
+            remain = total - current_sum
+            while ((remain - coins[i]) > 0 and
+                   (current_sum + coins[i]) < total):
+                current_sum += coins[i]
                 counter[coins[i]] += 1
-                remain = total - current
+                remain = total - current_sum
             if remain in coins:
-                current += remain
+                current_sum += remain
                 counter[coins[i]] += 1
                 return sum([count for count in counter.values()])
             elif remain % coins[i] == 0:
-                while (current != total):
-                    current += coins[i]
+                while (current_sum != total):
+                    current_sum += coins[i]
                     counter[coins[i]] += 1
                 return sum([count for count in counter.values()])
             else:
                 continue
+
     return -1
